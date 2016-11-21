54ed209258fb1efdb4345b1c93362c07e0408c75
Miguel Lopez <Miguel Lopez@Miguel>
2016-11-20 21:17:06 -0500

    implementacion de Gest_Inventory

---
+++ src/Modelos/Productos.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+/**
+ *
+ * @author Miguel Lopez
+ */
+public class Productos {
+     public Integer Id ;
+     public String Nombre;
+     public Integer P_Unitario;
+     public Integer Id_Proveedor;
+    }
