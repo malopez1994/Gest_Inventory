03b4e55a8c450c1d6e3778cb20120fee89b1535c
Miguel Lopez <Miguel Lopez@Miguel>
2016-11-20 21:17:19 -0500

    implementacion de Gest_Inventory

---
+++ src/Modelos/Proveedores.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+/**
+ *
+ * @author Miguel Lopez
+ */
+public class Proveedores {
+     public Integer Id ;
+     public String Nombre;
+     public String N_Contacto;
+     public String Direccion;
+     public String Telefono;
+     public String Correo;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer Id) {
+        this.Id = Id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getN_Contacto() {
+        return N_Contacto;
+    }
+
+    public void setN_Contacto(String N_Contacto) {
+        this.N_Contacto = N_Contacto;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+     
+ 
+     
+}
