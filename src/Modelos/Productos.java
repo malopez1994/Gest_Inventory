af95d5e2b8b995e03e28eee73581182eb0d1b90d
Miguel Lopez <Miguel Lopez@Miguel>
2016-11-24 22:20:57 -0500

    implementacion de Gest_Inventory - producto

--- src/Modelos/Productos.java
+++ src/Modelos/Productos.java
@@ -12,6 +12,38 @@ package Modelos;
 public class Productos {
      public Integer Id ;
      public String Nombre;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer Id) {
+        this.Id = Id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public Integer getP_Unitario() {
+        return P_Unitario;
+    }
+
+    public void setP_Unitario(Integer P_Unitario) {
+        this.P_Unitario = P_Unitario;
+    }
+
+    public Integer getId_Proveedor() {
+        return Id_Proveedor;
+    }
+
+    public void setId_Proveedor(Integer Id_Proveedor) {
+        this.Id_Proveedor = Id_Proveedor;
+    }
      public Integer P_Unitario;
      public Integer Id_Proveedor;
     }
